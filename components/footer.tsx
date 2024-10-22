ea98c158d1f4dabd8d6b1edc47c41cb54045a00d
MarcBTHT <marc.berthol29@gmail.com>
2024-10-22 18:35:44 +0200

    feat: :art: Update footer responsive

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,11 +2,11 @@ import Link from 'next/link';
 
 const Footer = () => {
     return (
-        <footer className="text-gray-800 z-10">
-            <div className="container mx-auto px-4 py-10">
+        <footer className="text-gray-800 w-full z-10">
+            <div className="w-full px-4 sm:w-11/12 md:w-10/12 lg:w-9/12 xl:w-8/12 mx-auto text-center justify-center py-10">
                 <div className="flex flex-wrap justify-between text-center">
                     <div className="w-full sm:w-1/4 mb-6 sm:mb-0">
-                        <h3 className="font-bold text-xl text-white mb-2">About Us</h3>
+                        <h3 className="font-bold text-lg text-white mb-2">About Us</h3>
                         <ul>
                             <li className="pb-1 text-gray-600 text-sm"><Link href="/about">Our Company</Link></li>
                             <li className="pb-1 text-gray-600 text-sm"><Link href="/careers">Careers</Link></li>
@@ -14,7 +14,7 @@ const Footer = () => {
                         </ul>
                     </div>
                     <div className="w-full sm:w-1/4 mb-6 sm:mb-0">
-                        <h3 className="font-bold text-xl text-white mb-2">Resources</h3>
+                        <h3 className="font-bold text-lg text-white mb-2">Resources</h3>
                         <ul>
                             <li className="pb-1 text-gray-600 text-sm"><Link href="/faq">FAQ</Link></li>
                             <li className="pb-1 text-gray-600 text-sm"><Link href="/support">Customer Support</Link></li>
@@ -22,7 +22,7 @@ const Footer = () => {
                         </ul>
                     </div>
                     <div className="w-full sm:w-1/4">
-                        <h3 className="font-bold text-xl text-white mb-2">Legal</h3>
+                        <h3 className="font-bold text-lg text-white mb-2">Legal</h3>
                         <ul>
                             <li className="pb-1 text-gray-600 text-sm"><Link href="/terms">Terms of Use</Link></li>
                             <li className="pb-1 text-gray-600 text-sm"><Link href="/privacy">Privacy Policy</Link></li>
