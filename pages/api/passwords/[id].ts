51a4fc6b719de8bf662adc09b2cffd79bf1aa903
MarcBTHT <marc.berthol29@gmail.com>
2024-10-22 16:52:41 +0200

    refactor: :white_check_mark: Hash passwords for secure storage
    With bcryptjs

--- pages/api/passwords/[id].ts
+++ pages/api/passwords/[id].ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next"; // Import des types
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs";
 
 const prisma = new PrismaClient();
 
@@ -8,10 +9,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
   if (req.method === "PUT") {
     const { service, username, password } = req.body;
+
+    // Hash the password before updating it
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // Update the password in the database
     const updatedPassword = await prisma.password.update({
-      where: { id: parseInt(id as string) }, // Utilisation de "as string" pour forcer le type
-      data: { service, username, password },
+      where: { id: parseInt(id as string) },
+      data: { service, username, password: hashedPassword },
     });
+
     res.status(200).json(updatedPassword);
   } else if (req.method === "DELETE") {
     await prisma.password.delete({
--- pages/api/passwords/index.ts
+++ pages/api/passwords/index.ts
@@ -1,18 +1,25 @@
 import { PrismaClient } from "@prisma/client";
-import { NextApiRequest, NextApiResponse } from "next"; // Import des types
+import { NextApiRequest, NextApiResponse } from "next";
+import bcrypt from "bcryptjs"; // Import bcrypt
 
 const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
     const { service, username, password } = req.body;
+
+    // Hash the password before storing it
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 is the salt rounds
+
+    // Store the hashed password in the database
     const newPassword = await prisma.password.create({
       data: {
         service,
         username,
-        password,
+        password: hashedPassword, // Store the hashed password
       },
     });
+    
     res.status(201).json(newPassword);
   } else if (req.method === "GET") {
     const passwords = await prisma.password.findMany();
